0315b8248bebb97cc1d3b5ac3cd51518ea966c7b
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-11-06 16:53:37 -0800

    Create a new post page.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -121,4 +121,8 @@ router.get('/cities/:id', (req, res) => {
   .catch((error) => next(error));
 })
 
+router.get('/newpost', (req, res) => {
+  return res.render('posts/new_post', { city: false })
+})
+
 module.exports = router;
