6ed21f70d09f9b4527f37ea239b81a51d4038217
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-11-15 11:40:46 -0800

    a user can see a pop-up that says Are you sure you want to delte titel? when clicking delete. A user can have the post deleted when confirming the pop-up.

--- public/script.js
+++ public/script.js
@@ -1,21 +1,12 @@
-console.log("Sanity Check~~~");
 const deleteButtons = document.getElementsByClassName('delete-button');
-
-console.log(":: ==>", deleteButtons);
 
 function confirmDelete(post) {
   const button = post.target;
-  console.log("button", button);
   const title = button.parentElement.previousElementSibling.previousElementSibling;
-  confirm(`Are you sure you want to delete ${title.innerText}?`)
-  // if (!confirm(`Are you sure you want to delete ${title.innerText}?`)) {
-  //   event.preventDefault()
-  // }
+  if (!confirm(`Are you sure you want to delete ${title.innerText}?`)) {
+    event.preventDefault()
+  }
 }
-
-// deleteButtons.forEach.call(document.getElementsByName("delete-button")[0], (link) => {
-//   link.addEventListener("click", confirmDelete)
-// })
 
 for (let i = 0; i < deleteButtons.length; i++) {
   deleteButtons[i].addEventListener('click', confirmDelete);
